063f1d033c96437865fbe68396f16db707202656
Sevki Hasirci <s@sevki.org>
2014-09-04 15:48:13 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,12 +2,6 @@ GCC=$ARCH-elf-gcc
 BIN=$ARCH-elf-binutils
 GDB=$ARCH-elf-gdb
 
-brew audit $BIN
-brew audit $GCC
-brew audit $GDB
-brew install -v $BIN
-brew install -v $GCC
-brew install -v $GDB
-brew test $BIN
-brew test $GCC
-brew test $GDB
+brew install $BIN
+brew install $GCC
+brew install $GDB
