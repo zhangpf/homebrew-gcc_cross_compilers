f6171db13d0c1200ec620f38b01021c4b68b9227
Yury Popov <github@phoenix.dj>
2016-08-21 14:35:54 +0300

    Update binutils to 2.27 (+SHA256)

--- x86_64-elf-binutils.rb
+++ x86_64-elf-binutils.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class X8664ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.gz'
-  sha1 'f10c64e92d9c72ee428df3feaf349c4ecb2493bd'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz'
+  sha256 '26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f'
 
   depends_on 'gcc' => :build
   def install
